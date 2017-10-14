b855c0f6727ea72010c210b78b9d19fcbf8da297
pham duc khanh <phamduckhanh2411@gmail.com>
2017-10-14 11:42:29 +0700

    create barebone for branch and order controller

---
+++ QuanLiBanHang/src/main/java/da/java/common/controller/BranchController.java
@@ -0,0 +1,22 @@
+package da.java.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("branch")
+public class BranchController {
+	
+	@GetMapping("/")
+    public String index(Model model) {
+        return "branch.index";
+    }
+	
+	@GetMapping("/{id}")
+    public String details(@PathVariable int id, Model model) {
+        return "branch.details";
+    }
+}
