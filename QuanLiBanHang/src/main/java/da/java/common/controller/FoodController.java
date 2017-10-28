eb4ca9b9588995a10f26e9d54c59c3e538a6f2a9
pham duc khanh <phamduckhanh2411@gmail.com>
2017-10-28 16:21:00 +0700

    implement list food page

--- QuanLiBanHang/src/main/java/da/java/common/controller/FoodController.java
+++ QuanLiBanHang/src/main/java/da/java/common/controller/FoodController.java
@@ -1,10 +1,16 @@
 package da.java.common.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import da.java.common.entities.Food;
+import da.java.common.service.FoodService;
 
 
 
@@ -12,8 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("food")
 public class FoodController {
 	
+	@Autowired
+    private FoodService foodService;
+	
 	@GetMapping("/")
     public String index(Model model) {
+		List<Food> listFood = foodService.getFoodList();
+		model.addAttribute("listFood", listFood);
         return "food/index";
     }
 	
