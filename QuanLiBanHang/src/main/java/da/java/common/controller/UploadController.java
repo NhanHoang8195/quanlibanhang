354e68704b3ba03bbe36c445913b4d15d97f8fd9
pham duc khanh <phamduckhanh2411@gmail.com>
2017-11-26 22:13:31 +0700

    upload file for food

---
+++ QuanLiBanHang/src/main/java/da/java/common/controller/UploadController.java
@@ -0,0 +1,78 @@
+package da.java.common.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import da.java.common.entities.Category;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.ServletContext;
+
+@Controller
+public class UploadController {
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "c://temp//";
+    
+    @Autowired
+    ServletContext context;
+    
+    private String getFileExtension(String name) {
+        try {
+            return name.substring(name.lastIndexOf(".") + 1);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+    
+    @PostMapping("/upload/") // //new annotation since 4.3
+    public ResponseEntity<?> singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        try {
+        	File UPLOAD_DIR = new File(UPLOADED_FOLDER);
+        	 if (!UPLOAD_DIR.exists()) {
+        		 UPLOAD_DIR.mkdirs();
+             }
+        	  byte[] bytes = file.getBytes();
+        	  
+            // Get the file and save it somewhere
+        	String name = java.util.UUID.randomUUID().toString();
+        	String extension = getFileExtension(file.getOriginalFilename());
+            Path path = Paths.get(UPLOADED_FOLDER + name + "." + extension);
+            
+            Files.write(path, bytes);
+            
+            Category cate = new Category();
+            cate.setName("/imageUploaded/" + name + "/" + extension);
+            return new ResponseEntity<>(cate, HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+		return null;
+    }
+    @RequestMapping(value = "/imageUploaded/{imageName}/{extension}")
+    @ResponseBody
+    public byte[] getImage(@PathVariable(value = "imageName") String imageName,
+    		@PathVariable(value = "extension") String extension) throws IOException {
+        File serverFile = new File(UPLOADED_FOLDER + imageName + "." + extension);
+
+        return Files.readAllBytes(serverFile.toPath());
+    }
+}
--- QuanLiBanHang/src/main/resources/static/js/adminController.js
+++ QuanLiBanHang/src/main/resources/static/js/adminController.js
@@ -8,9 +8,26 @@ $(document).ready(function(c) {
          element.addClass('active').parent().parent('li').addClass('active')
      }
 });
-
+function resetAllFiles() {
+    var inputs = document.getElementsByTagName('input');
+    var length = inputs.length;
+    for (i = 0; i < length; i++) {
+        if (inputs[i].type == "file") {
+            inputs[i].value = "";
+        }
+    }
+}
 var app = angular.module('myApp', []);
 
+angular.module('myApp').directive('customOnChange', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var onChangeHandler = scope.$eval(attrs.customOnChange);
+            element.bind('change', onChangeHandler);
+        }
+    };
+});
 function defineBaseFunction(scope, http)
 {
 	scope.currentModel = {};
@@ -55,6 +72,22 @@ function defineBaseFunction(scope, http)
 			scope.get();
 		})
 	}
+	scope.addFile= function (event) {
+        var file = event.target.files[0];
+
+        var formData = new FormData();
+        formData.append('file', file);
+        http.post("/upload/", formData, {
+            transformRequest : angular.identity,
+            headers : {
+                'Content-Type' : undefined
+            }})
+		.then(function(response) {
+			scope.currentModel.image = response.data.name;
+
+			resetAllFiles();
+		})
+    };
 }
 
 app.controller('foodController', function($scope, $http) {
