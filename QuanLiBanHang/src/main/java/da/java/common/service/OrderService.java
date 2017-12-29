45d6d2d63b5909437a041a6a080b6c558f7426c3
DESKTOP-ABATC2R\Ngoc Hai <pnhai.vn@gmail.com>
2017-12-29 16:01:05 +0700

    Update service get list order

--- QuanLiBanHang/src/main/java/da/java/common/service/OrderService.java
+++ QuanLiBanHang/src/main/java/da/java/common/service/OrderService.java
@@ -1,5 +1,7 @@
 package da.java.common.service;
 
+import java.util.List;
+
 import da.java.common.entities.Order;
 
 public interface OrderService {
@@ -7,4 +9,5 @@ public interface OrderService {
     public Order saveOrder(String json);
     
     public Void deleteOrder(Long id);
+    public List<Order> getOrderList();
 }
